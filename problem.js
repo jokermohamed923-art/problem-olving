d31d5c0e602e42e58797cd0f6530c8ae10a2f8ed
mohamed <jokermohamed923@gmail.com>
2025-11-08 08:11:13 +0200

    problem1

---
+++ problem.js
@@ -0,0 +1,6 @@
+// create a function that task an integer as an argument and returns "even" for "even" number 
+// or "odd" for odd numbers
+function even(value){
+return value % 2 ===0 ? "Event":"odd"
+}
+console.log(even(4))
