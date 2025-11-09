e3e2a91c994ef4b034ff8ad65db1d21604b1e1a5
mohamed <jokermohamed923@gmail.com>
2025-11-09 16:28:51 +0200

    write function called repeat string  which repeats string axactly

---
+++ problem3.js
@@ -0,0 +1,10 @@
+// write function called repeat string  which repeats string axactly
+function repeatestring(string,number){
+    let inilvalue =""
+    for(let i= 0 ; i < number;i++){
+       inilvalue +=[i]+" "+string + " ===>>"
+    }
+  
+return  inilvalue 
+}
+console.log(repeatestring("halo",7))
