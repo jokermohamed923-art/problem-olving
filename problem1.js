07cd1dc4db0b764fb082b985aa99f070d3c42a08
mohamed <jokermohamed923@gmail.com>
2025-11-09 12:24:45 +0200

    add problem solving

--- problem1.js
+++ problem1.js
@@ -1,11 +1,10 @@
-// you get array of numbers returnthe sum of all positives ones
+// you get array of numbers return the sum of all positives ones
 function sumOfpositive(array){
     let initvalue=0
 for(let i = 0; i<array.length;i++){
     if(array[i] > 0 ){
         initvalue +=array[i]
-    }
-}
+    } }
 return initvalue
 
 }
